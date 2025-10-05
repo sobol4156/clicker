dd96fbd7b13642569c61c2e5c5f0f329aa8a99ba
alex <sobol4156@gmail.com>
2025-10-06 01:39:53 +0300

    feat: added animation tap

--- src/app/modules/clicker/clicker.component.ts
+++ src/app/modules/clicker/clicker.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { timer } from 'rxjs';
+
+interface Popup {
+  id: number;
+  value: number;
+  x: number;
+  y: number;
+}
 
 @Component({
   selector: 'app-clicker',
@@ -7,8 +15,29 @@ import { Component } from '@angular/core';
 })
 export class ClickerComponent {
   myCoins = 0
+  coinBonus = 1
+  popups: Popup[] = [];
 
-  increment(){
-    this.myCoins += 1
+  increment(event: MouseEvent) {
+    this.myCoins += this.coinBonus
+
+    const target = (event.currentTarget as HTMLElement).getBoundingClientRect();
+    const x = event.clientX - target.left - 10;
+    const y = event.clientY - target.top - 20;
+
+    const newPopup: Popup = {
+      id: Date.now(),
+      value: 1,
+      x,
+      y
+    }
+
+    this.popups.push(newPopup)
+
+    timer(1000).subscribe(() => {
+      this.popups = this.popups.filter(p => p.id !== newPopup.id)
+    })
   }
+
+  
 }
