2f8c776f7620358dc9de9fa51b413ec18ce668af
alex <sobol4156@gmail.com>
2025-10-06 16:25:18 +0300

    fix: btn animation tap

--- src/app/shared/ui/button/button.component.ts
+++ src/app/shared/ui/button/button.component.ts
@@ -1,10 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss']
 })
-export class ButtonComponent {
-  @Input() type: 'default' = 'default'
+export class ButtonComponent implements AfterViewInit {
+  @Input() type: 'default' = 'default';
+  @Output() click = new EventEmitter<Event>();
+
+  @ViewChild('button', { static: true }) btn!: ElementRef<HTMLButtonElement>;
+
+  ngAfterViewInit() {
+    const button = this.btn.nativeElement;
+
+    button.addEventListener('click', (event) => this.click.emit(event));
+
+    button.addEventListener('touchstart', () => button.classList.add('active'));
+    button.addEventListener('touchend', () => button.classList.remove('active'));
+  }
 }
